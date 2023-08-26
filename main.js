1b329dbc06f43867c8a64fe28043abed37281938
Ogurabike <yoshidago@ogurabike.com>
2023-08-27 04:39:27 +0900

    Ver2.2 一回計算した後パラメーを変えずに再計算を実行するとエラーが出る不具合を修正

--- main.js
+++ main.js
@@ -24,13 +24,13 @@ function F1_setVal() {
 
 //テーブル値の取得
 function F1_getVal(obt) {
-  obt.cp = parseFloat(document.getElementById("F1_label_cp").innerText);      //チェーンピッチ
-  obt.r0 = parseFloat(document.getElementById("F1_input_r0").value);          //チェーンテンショナースイング半径
-  obt.r1 = parseFloat(document.getElementById("hidden_F1_label_d1").value) / 2;  //チェーンリングピッチ円半径
-  obt.r2 = parseFloat(document.getElementById("hidden_F1_label_d2").value) / 2;  //スプロケットピッチ円半径
-  obt.r3 = parseFloat(document.getElementById("hidden_F1_label_d3").value) / 2;  //テンションプーリーピッチ円半径
-  obt.d0 = parseFloat(document.getElementById("hidden_F1_label_d0").value);		  //中心間最小距離(チェーンステー⇔プーリー)
-  obt.dlcs = parseFloat(document.getElementById("F1_input_dlcs").value);      //チェーンステー長
+  obt.cp = parseFloat(document.getElementById("F1_label_cp").innerText);          //チェーンピッチ
+  obt.r0 = parseFloat(document.getElementById("F1_input_r0").value);              //チェーンテンショナースイング半径
+  obt.r1 = parseFloat(document.getElementById("hidden_F1_label_d1").value) / 2;   //チェーンリングピッチ円半径
+  obt.r2 = parseFloat(document.getElementById("hidden_F1_label_d2").value) / 2;   //スプロケットピッチ円半径
+  obt.r3 = parseFloat(document.getElementById("hidden_F1_label_d3").value) / 2;   //テンションプーリーピッチ円半径
+  obt.d0 = parseFloat(document.getElementById("hidden_F1_label_d0").value);		    //中心間最小距離(チェーンステー⇔プーリー)
+  obt.dlcs = parseFloat(document.getElementById("F1_input_dlcs").value);          //チェーンステー長
 
   //半コマチェーン
   let elements = document.getElementsByName("F1_input_chainTypes");
@@ -58,7 +58,7 @@ function startCalcAndOpti() {
   let returnMsg;
 
   let obt1 = new orbit;
-  F1_getVal(obt1);
+  F1_getVal(obt1); // 軌道計算ボタンクリックを最初に押したときはtrue、２回目以降はfalse
   if (obt1.pulleyPositionCheck(1) == false) {
     returnMsg = "	d'0中心間最小距離(チェーンステー⇔チェーンピン)が小さすぎます。上側チェーンと下側チェーンが干渉してしまいます。";
     document.getElementById("F1_label_returnMsg").innerText = returnMsg;
@@ -80,54 +80,69 @@ function startCalcAndOpti() {
   obt1.setOrbit();
 
   //仮計算した軌道からチェーンのコマ数を決定
+  let nc0 = obt1.x.length-1;
   let nc = obt1.optimumLinkNumber();
 
-  if (nc < nc_min) {
-    nc = nc_min;
-  }
-
-  //最適化されたチェーンのコマ数をもとに、テンションプーリーの位置を最適化(ここでobt1のチェーン軌道が最終決定される)
-  let d00 = obt1.d0;
-  if ((obt1.set_d0Min(nc)) && !(obt1.d0 == d00)) {
-    returnMsg = "\n\nなお、d'0中心間最小距離(チェーンステー⇔チェーンピン)は、以下の通り最適化されています。\n"
-      + varFormat(d00 - obt1.r3,"0.0") + "mm→" + varFormat(obt1.d0 - obt1.r3,"0.0") + "mm";
-
-    //d'0中心間最小距離(チェーンステー⇔チェーンピン)を再設定
-    document.getElementById("F1_input_dd0").value = obt1.d0 - obt1.r3;  // r3 = d3/2
-    F1_setVal();  // onchangeイベントを強制実行
+  // 軌道計算ボタンクリックを最初に押したときはfalse、２回目以降はtrue
+  let calcFlg;
+  if (document.getElementById("F1_label_returnMsg").innerText == "") {
+    calcFlg = false;
   } else {
-    returnMsg = "最適化に失敗しました。\n	d'0中心間最小距離(チェーンステー⇔チェーンピン)を見直してください。";
-    document.getElementById("F1_label_returnMsg").innerText = returnMsg;
+    calcFlg = true;
+  }
+
+  // 計算２回目以降かつ仮計算軌道のチェーンコマ数に修正が不要な場合は
+  // 前回計算からパラメータ変更なしみなし、これ以上の再計算は行わない。
+  if ((nc == nc0) && calcFlg) {
     return;
+  } else {
+    if (nc < nc_min) {
+      nc = nc_min;
+    }
+
+    //最適化されたチェーンのコマ数をもとに、テンションプーリーの位置を最適化(ここでobt1のチェーン軌道が最終決定される)
+    let d00 = obt1.d0;
+    if ((obt1.set_d0Min(nc)) && !(obt1.d0 == d00)) {
+      returnMsg = "\n\nなお、d'0中心間最小距離(チェーンステー⇔チェーンピン)は、以下の通り最適化されています。\n"
+        + varFormat(d00 - obt1.r3,"0.0") + "mm→" + varFormat(obt1.d0 - obt1.r3,"0.0") + "mm";
+
+      //d'0中心間最小距離(チェーンステー⇔チェーンピン)を再設定
+      document.getElementById("F1_input_dd0").value = obt1.d0 - obt1.r3;  // r3 = d3/2
+      F1_setVal();  // onchangeイベントを強制実行
+    } else {
+      returnMsg = "最適化に失敗しました。\n	d'0中心間最小距離(チェーンステー⇔チェーンピン)を見直してください。";
+      document.getElementById("F1_label_returnMsg").innerText = returnMsg;
+      return;
+    }
+    
+    //吸収可能なチェーンステー増加量を計算
+    //チェーンがパツパツになるまでチェーンステー長を伸ばした状態のシミュレーション
+    let obt2 = new orbit;
+    F1_getVal(obt2);
+    obt2.setOrbit_dlcsMax(nc);
+
+    //obt1と0bt2のチェーンステー長の差が0.1mm以下になった時は最初からパツパツだったこととする。
+    let dlcs0 = obt1.dlcs;
+    if (obt2.dlcs - obt1.dlcs < 0.1 ) {
+      obt1 = obt2;
+      obt1.dlcs = dlcs0;
+    }
+
+    returnMsg = "上記セッティングにより、最大" + varFormat(obt2.dlcs - obt1.dlcs,"0.0") 
+      + "mmのチェーンステー長の増加が吸収可能です。"
+      + returnMsg;
+
+    //計算結果表示
+    setResultTable(obt1,obt2);
+    
+    //軌道描画
+    drawChart(obt1,obt2);
+
+    //チェーンテンショナースイング角算出
+    setDeflection();
+
+    document.getElementById("F1_label_returnMsg").innerText = returnMsg;
   }
-  
-  //吸収可能なチェーンステー増加量を計算
-  //チェーンがパツパツになるまでチェーンステー長を伸ばした状態のシミュレーション
-  let obt2 = new orbit;
-  F1_getVal(obt2);
-  obt2.setOrbit_dlcsMax(nc);
-
-  //obt1と0bt2のチェーンステー長の差が0.1mm以下になった時は最初からパツパツだったこととする。
-  let dlcs0 = obt1.dlcs;
-  if (obt2.dlcs - obt1.dlcs < 0.1 ) {
-    obt1 = obt2;
-    obt1.dlcs = dlcs0;
-  }
-
-  returnMsg = "上記セッティングにより、最大" + varFormat(obt2.dlcs - obt1.dlcs,"0.0") 
-    + "mmのチェーンステー長の増加が吸収可能です。"
-    + returnMsg;
-
-  //計算結果表示
-  setResultTable(obt1,obt2);
-  
-  //軌道描画
-  drawChart(obt1,obt2);
-
-  //チェーンテンショナースイング角算出
-  setDeflection();
-
-  document.getElementById("F1_label_returnMsg").innerText = returnMsg;
 }
 
 //計算結果表示
